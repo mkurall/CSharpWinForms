7a53d71cc62eaad13efc29fce7af0f2cfe40f22e
Mustafa KURAL <mustafakural@outlook.com>
2024-03-16 09:24:55 +0300

    EF Test2 Oluşturuldu

---
+++ EFTest2/Models/OkulDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFTest2.Models
+{
+    public class OkulDbContext : DbContext
+    {
+        public OkulDbContext()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=btk_okul_db;Trusted_Connection=True;Encrypt=False");
+        }
+
+        public virtual DbSet<Sinif> Siniflar { get; set; }
+    }
+}
+    //Add-Migration IlkVersiyon
+    //Update-Database
